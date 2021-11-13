e5235dff5e4d197f5a873e3b858a2ecb694140a7
Quochung1st-dev <Quochung1st-dev@users.noreply.github.com>
2021-11-13 11:49:56 +0700

    fix erro image

--- public/js/flu.js
+++ public/js/flu.js
@@ -98,16 +98,16 @@
 });
 
 var assets = [
-    'images/1.png',
-    'images/2.png',
-    'images/3.png',
-    'images/4.png',
-    'images/5.png',
-    'images/6.png',
-    'images/7.png',
-    'images/8.png',
-    'images/9.png',
-    'images/10.png'
+    // 'images/1.png',
+    // 'images/2.png',
+    // 'images/3.png',
+    // 'images/4.png',
+    // 'images/5.png',
+    // 'images/6.png',
+    // 'images/7.png',
+    // 'images/8.png',
+    // 'images/9.png',
+    // 'images/10.png'
 ];
 assets.forEach(function(src, index) {
     assets[index] = new Promise(function(resolve) {
